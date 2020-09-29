f08c66c3e7dc18fdffb1bc9147ed1188605916be
yeonghak <yongxue1215@126.com>
2020-09-29 08:09:44 +0800

    Create .eslintrc_ph24center.js

---
+++ .eslintrc_ph24center.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "jquery": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 5,
+        "sourceType": "script"
+    },
+    "extends": [],
+    "rules": {
+        "semi": "error",
+        "no-extra-semi": "error",
+        "semi-spacing": "error",
+        "semi-style": "error"
+    }
+};
